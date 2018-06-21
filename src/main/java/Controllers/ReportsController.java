8c00f7096c57faf0c1596835ae1c282c901f0b57
ovidiu <enescu_vd@yahoo.com>
2018-06-21 23:47:56 +0300

    added barchart rates

--- src/main/java/Controllers/ReportsController.java
+++ src/main/java/Controllers/ReportsController.java
@@ -1,12 +1,16 @@
 package Controllers;
 
 import helpers.db.ExpenseDBHelper;
+import helpers.db.RateDBHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import model.Expense;
+import model.Rate;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,12 +21,17 @@ import java.util.ResourceBundle;
 public class ReportsController implements Initializable {
     @FXML
     PieChart pieExpenses;
+    @FXML
+    BarChart<String, Number> barChartRates;
+
 
     private ExpenseDBHelper expenseDBHelper = new ExpenseDBHelper();
+    private RateDBHelper rateDBHelper = new RateDBHelper();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setUpPieChartExpenses();
+        setUpBarChartRates();
     }
 
     private void setUpPieChartExpenses() {
@@ -32,5 +41,18 @@ public class ReportsController implements Initializable {
             pieChartExpensesData.add(new PieChart.Data(e.getTitle(), e.getAmount()));
         }
         pieExpenses.setData(pieChartExpensesData);
+        pieExpenses.setAnimated(true);
+        pieExpenses.setLabelsVisible(true);
+    }
+
+    private void setUpBarChartRates() {
+        ObservableList<Rate> rateList = FXCollections.observableArrayList(rateDBHelper.fetchAll());
+
+        XYChart.Series series = new XYChart.Series();
+        for (Rate rate : rateList) {
+            series.getData().add(new XYChart.Data<>(rate.getAmount().toString(), rate.getAmount()));
+        }
+        barChartRates.getData().add(series);
+
     }
 }
