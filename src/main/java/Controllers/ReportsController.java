16a8fa3e2fad0e96e47287d340068e6b032048fe
ovidiu <enescu_vd@yahoo.com>
2018-06-24 09:52:58 +0300

    set up stacked bar chart

--- src/main/java/Controllers/ReportsController.java
+++ src/main/java/Controllers/ReportsController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
 import model.Expense;
 import model.Rate;
@@ -23,6 +24,8 @@ public class ReportsController implements Initializable {
     PieChart pieExpenses;
     @FXML
     BarChart<String, Number> barChartRates;
+    @FXML
+    StackedBarChart<String, Number> stackedBarChart;
 
 
     private ExpenseDBHelper expenseDBHelper = new ExpenseDBHelper();
@@ -32,6 +35,7 @@ public class ReportsController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         setUpPieChartExpenses();
         setUpBarChartRates();
+        setUpStackedBarChart();
     }
 
     private void setUpPieChartExpenses() {
@@ -46,12 +50,35 @@ public class ReportsController implements Initializable {
     }
 
     private void setUpBarChartRates() {
-        ObservableList<Rate> rateList = FXCollections.observableArrayList(rateDBHelper.fetchAll());
+        ObservableList<Expense> expensesList = FXCollections.observableArrayList(expenseDBHelper.fetchAllWithRates());
 
-        XYChart.Series series = new XYChart.Series();
-        for (Rate rate : rateList) {
-            series.getData().add(new XYChart.Data<>(rate.getAmount().toString(), rate.getAmount()));
+        for (Expense expense : expensesList) {
+            XYChart.Series series = new XYChart.Series();
+            series.setName(expense.getTitle());
+            if (expense.getPayedRates().size() > 0) {
+                for (Rate rate : expense.getPayedRates()) {
+                    series.getData().add(new XYChart.Data<>(rate.getAmount().toString(), rate.getAmount()));
+                }
+                barChartRates.getData().add(series);
+            }
         }
-        barChartRates.getData().add(series);
+        barChartRates.setBarGap(5);
+
+    }
+
+    private void setUpStackedBarChart() {
+        ObservableList<Expense> expensesList = FXCollections.observableArrayList(expenseDBHelper.fetchAllWithRates());
+
+        for (Expense expense : expensesList) {
+            XYChart.Series series = new XYChart.Series();
+            series.setName(expense.getTitle());
+            if (expense.getPayedRates().size() > 0) {
+                for (Rate rate : expense.getPayedRates()) {
+                    XYChart.Data<String, Double> e = new XYChart.Data<>(expense.getTitle(), rate.getAmount());
+                    series.getData().add(e);
+                }
+                stackedBarChart.getData().add(series);
+            }
+        }
     }
 }
