a1c0dced107574eec2f4f3710004c2cef7f6076e
ovidiu <enescu_vd@yahoo.com>
2018-06-24 12:19:01 +0300

    added controller addCategoryModal

---
+++ src/main/java/Controllers/modals/AddCategoryController.java
@@ -0,0 +1,18 @@
+package Controllers.modals;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Ovidiu on 24-Jun-18.
+ */
+public class AddCategoryController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
